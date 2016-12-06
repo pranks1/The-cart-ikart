3129739b9db0f38681b3daf47c000cf61abfd43e
priyanka <priiyaaa5@gmail.com>
2016-12-06 13:12:01 +0530

    changed the HTML

--- Catchdeal/views.py
+++ Catchdeal/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def Product_list(request):
+    products = Product.objects.all()
+    return render(request, 'Catchdeal/product_list.html', {'products': products})
