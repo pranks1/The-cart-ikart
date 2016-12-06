68d8f3a6a12d3aee89f874a4b43b646cc71b9353
priyanka <priiyaaa5@gmail.com>
2016-12-06 18:48:10 +0530

    Modified templates to dispaly product list

--- Catchdeal/views.py
+++ Catchdeal/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Product
+
 # Create your views here.
 def Product_list(request):
     products = Product.objects.all()
     return render(request, 'Catchdeal/product_list.html', {'products': products})
+
+
+def product_detail(request):
+    product = Product.objects.get(pk=)
+    return render(request, 'Catchdeal/product_detail.html', {'products': products})
